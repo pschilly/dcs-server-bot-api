13e2b6e488246aa33da48c913f4914ba4b1e9f0c
Schilly <patrick.schillemat@gmail.com>
2025-08-19 23:46:43 -0400

    add option json or collect return

--- src/DcsServerBotApi.php
+++ src/DcsServerBotApi.php
@@ -53,14 +53,17 @@ class DcsServerBotApi
      * @api_endpoint /serverstats
      *
      * @param  string|null  $server_name  - limit the response to a specific server in your cluster.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getServerStats(?string $server_name = null): array
+    public static function getServerStats(?string $server_name = null, string $returnType = 'json'): mixed
     {
         $response = Http::baseUrl(self::getBaseUrl())->get('/serverstats', [
             'server_name' => $server_name,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -85,12 +88,15 @@ class DcsServerBotApi
      *
      * @api_endpoint /servers
      *
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getServerList(): array
+    public static function getServerList(string $returnType = 'json'): mixed
     {
         $response = Http::baseUrl(self::getBaseUrl())->get('/servers');
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -100,15 +106,18 @@ class DcsServerBotApi
      *
      * @api_endpoint /squadrons
      *
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getSquadronList(?int $limit = null, ?int $offset = null): array
+    public static function getSquadronList(?int $limit = null, ?int $offset = null, string $returnType = 'json'): mixed
     {
         $response = Http::baseUrl(self::getBaseUrl())->get('/squadrons', [
             'limit' => $limit,
             'offset' => $offset,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -119,12 +128,15 @@ class DcsServerBotApi
      * @api_endpoint /squadron_members
      *
      * @param  string  $squadronName  [required]
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getSquadronMembers(string $squadronName): array
+    public static function getSquadronMembers(string $squadronName, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/squadron_members', ['name' => $squadronName]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -135,12 +147,15 @@ class DcsServerBotApi
      * @api_endpoint /getuser
      *
      * @param  string  $nick  [required|wild]
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getUser(string $nick): array
+    public static function getUser(string $nick, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/getuser', ['nick' => $nick]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -152,20 +167,23 @@ class DcsServerBotApi
      *
      * @param  string  $discord_id  [required] - Discord ID of the player.
      * @param  bool|null  $force  - Force the operation.
-     * @return array as json
+     * @return mixed json|collection
      *               {
      *               "rc": 2,
      *               "timestamp": "2025-08-09T12:00:00+00:00",
      *               "token": "1234"
      *               }
      */
-    public static function linkMe(string $discord_id, ?bool $force = null): array
+    public static function linkMe(string $discord_id, ?bool $force = null, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/linkme', [
             'discord_id' => $discord_id,
             'force' => $force,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -177,15 +195,18 @@ class DcsServerBotApi
      *
      * @param  string  $nick  [required] - Actual name of the player. Get with $this->getUser($nick)
      * @param  string|null  $date  - Limit the response to a specific date.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getPlayerSquadrons(string $nick, ?string $date = null): array
+    public static function getPlayerSquadrons(string $nick, ?string $date = null, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/player_squadrons', [
             'nick' => $nick,
             'date' => $date,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -197,9 +218,18 @@ class DcsServerBotApi
      *
      * @param  int|null  $limit  - Limit the returned results to a specific number.
      * @param  string|null  $server_name  - limit the response to a specific server in your cluster.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getTopKills(?string $server_name = null, ?int $limit = 10, ?int $offset = null): array
+    /**
+     * Get top kills for a server.
+     *
+     * @param string|null $server_name
+     * @param int|null $limit
+     * @param int|null $offset
+     * @param string $returnType 'json' (default) or 'collection'
+     * @return array|\Illuminate\Support\Collection
+     */
+    public static function getTopKills(?string $server_name = null, ?int $limit = 10, ?int $offset = null, string $returnType = 'json'): mixed
     {
         $response = Http::baseUrl(self::getBaseUrl())->get('/topkills', [
             'server_name' => $server_name,
@@ -207,6 +237,9 @@ class DcsServerBotApi
             'offset' => $offset,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -218,9 +251,9 @@ class DcsServerBotApi
      *
      * @param  int|null  $limit  - Limit the returned results to a specific number.
      * @param  string|null  $server_name  - limit the response to a specific server in your cluster.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getTopKDR(?string $server_name = null, ?int $limit = 10, ?int $offset = null): array
+    public static function getTopKDR(?string $server_name = null, ?int $limit = 10, ?int $offset = null, string $returnType = 'json'): mixed
     {
         $response = Http::baseUrl(self::getBaseUrl())->get('/topkdr', [
             'server_name' => $server_name,
@@ -228,6 +261,9 @@ class DcsServerBotApi
             'offset' => $offset,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -240,9 +276,9 @@ class DcsServerBotApi
      * @api_endpoint /trueskill
      *
      * @param  string|null  $server_name  - limit the response to a specific server in your cluster.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getTrueSkillStats(?string $server_name = null, ?int $limit = 10, ?int $offset = null): array
+    public static function getTrueSkillStats(?string $server_name = null, ?int $limit = 10, ?int $offset = null, string $returnType = 'json'): mixed
     {
         $response = Http::baseUrl(self::getBaseUrl())->get('/trueskill', [
             'server_name' => $server_name,
@@ -250,6 +286,9 @@ class DcsServerBotApi
             'offset' => $offset,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -262,9 +301,9 @@ class DcsServerBotApi
      * @param  string  $nick  [required] - Actual nickname of the user. Get with $this->getUser($nick)
      * @param  string|null  $date  - Limit the response to a specific date.
      * @param  string|null  $server_name  - limit the response to a specific server in your cluster.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getWeaponPK(?string $server_name, string $nick, ?string $date = null): array
+    public static function getWeaponPK(?string $server_name, string $nick, ?string $date = null, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/weaponpk', [
             'nick' => $nick,
@@ -272,6 +311,9 @@ class DcsServerBotApi
             'date' => $date,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -284,9 +326,9 @@ class DcsServerBotApi
      * @param  string  $nick  [required] - Actual nickname of the user. Get with $this->getUser($nick)
      * @param  string|null  $date  - Limit the response to a specific date.
      * @param  string|null  $server_name  - limit the response to a specific server in your cluster.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getStats(?string $server_name, string $nick, ?string $date = null): array
+    public static function getStats(?string $server_name, string $nick, ?string $date = null, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/stats', [
             'nick' => $nick,
@@ -294,6 +336,9 @@ class DcsServerBotApi
             'server_name' => $server_name,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -306,9 +351,9 @@ class DcsServerBotApi
      * @param  string  $nick  [required] - Actual nickname of the user. Get with $this->getUser($nick)
      * @param  string|null  $date  - Limit the response to a specific date.
      * @param  string|null  $server_name  - limit the response to a specific server in your cluster.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getPlayerInfo(?string $server_name, string $nick, ?string $date = null): array
+    public static function getPlayerInfo(?string $server_name, string $nick, ?string $date = null, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/player_info', [
             'nick' => $nick,
@@ -316,6 +361,9 @@ class DcsServerBotApi
             'date' => $date,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -328,9 +376,9 @@ class DcsServerBotApi
      * @param  string|null  $server_name  - Limit the response to a specific server in your cluster.
      * @param  string|null  $period  - Limit the response to a specific time period.
      * @param  int|null  $limit  - Limit the number of results returned.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getHighscore(?string $server_name = null, ?int $limit = 10, ?string $period = null): array
+    public static function getHighscore(?string $server_name = null, ?int $limit = 10, ?string $period = null, string $returnType = 'json'): mixed
     {
         $response = Http::baseUrl(self::getBaseUrl())->get('/highscore', [
             'server_name' => $server_name,
@@ -338,6 +386,9 @@ class DcsServerBotApi
             'period' => $period,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -351,9 +402,9 @@ class DcsServerBotApi
      * @param  string|null  $date  - Limit the response to a specific date.
      * @param  int|null  $limit  - Limit the number of results returned.
      * @param  string|null  $server_name  - limit the response to a specific server in your cluster.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getTraps(?string $server_name = null, string $nick, ?int $limit = 10, ?int $offset = null, ?string $date = null): array
+    public static function getTraps(string $nick, ?string $server_name = null, ?int $limit = 10, ?int $offset = null, ?string $date = null, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/traps', [
             'nick' => $nick,
@@ -363,6 +414,9 @@ class DcsServerBotApi
             'server_name' => $server_name,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -375,9 +429,9 @@ class DcsServerBotApi
      * @param  string  $nick  [required] - Actual nickname of the user. Get with $this->getUser($nick)
      * @param  string|null  $date  - Limit the response to a specific date.
      * @param  string|null  $campaign  - limit the response to a specific campaign.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getCredits(string $nick, ?string $date = null, ?string $campaign = null): array
+    public static function getCredits(string $nick, ?string $date = null, ?string $campaign = null, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/credits', [
             'nick' => $nick,
@@ -385,6 +439,9 @@ class DcsServerBotApi
             'campaign' => $campaign,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 
@@ -396,15 +453,18 @@ class DcsServerBotApi
      *
      * @param  string  $name  [required] - Actual name of the squadron.
      * @param  string|null  $campaign  - limit the response to a specific campaign.
-     * @return array as json
+     * @return mixed json|collection
      */
-    public static function getSquadronCredits(string $name, ?string $campaign = null): array
+    public static function getSquadronCredits(string $name, ?string $campaign = null, string $returnType = 'json'): mixed
     {
         $response = Http::asForm()->baseUrl(self::getBaseUrl())->post('/squadron_credits', [
             'name' => $name,
             'campaign' => $campaign,
         ]);
 
+        if ($returnType === 'collection') {
+            return collect($response->json());
+        }
         return $response->json();
     }
 }
